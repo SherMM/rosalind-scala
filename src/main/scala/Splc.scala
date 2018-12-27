76f5413a957d8014759ecc898b1fa8cfca2f07c0
Ian Laurain <i599914@8451.com>
2018-12-27 14:20:26 -0500

    finished rna splice problem

---
+++ src/main/scala/Splc.scala
@@ -0,0 +1,24 @@
+import Utils.readFastaStrandsFromFile
+import Rna.transcribeDnaToRna
+import Prot.translateRnaToProtein
+
+object Splc {
+
+    def getSplicedProtein(dna: String, introns: List[String]): String = {
+        if (introns.isEmpty) {
+            dna
+        } else {
+            val (intron, rest) = (introns.head, introns.tail)
+            getSplicedProtein(dna.replace(intron, ""), rest) 
+        }
+    }
+
+    def solveSplc(filename: String): Unit = {
+        val strands = readFastaStrandsFromFile(filename).toList
+        val (dna, introns) = (strands.head, strands.tail)
+        val spliced = getSplicedProtein(dna, introns)
+        val rna = transcribeDnaToRna(spliced)
+        println(translateRnaToProtein(rna))
+    }
+
+}
